483f6ed24983685445e502132d1b8c93fdd8dda9
J.M. Thiessen <jacob@x0rz3q.com>
2021-07-01 11:32:39 +0200

    Adds OTF convert script

---
+++ convert_otf.py
@@ -0,0 +1,20 @@
+import fontforge
+import os
+from shutil import copyfile
+
+print('[+] Creating directory')
+os.makedirs('ttf', exist_ok=True)
+
+for root, dirs, files in os.walk('fonts'):
+    for f in files:
+        name, extension = os.path.splitext(f)
+
+        path = os.path.join(root, f)
+        if extension == '.ttf':
+            print(f"[+] Copying {path!r}")
+            copyfile(path, os.path.join('ttf', f))
+        elif extension == '.otf':
+            print(f"[+] Converting {path!r}")
+            fontforge.open(path).generate(os.path.join('ttf', name + '.otf'))
+        else:
+            print(f"[-] Extension not recognized {extension!r}")
